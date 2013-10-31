f4411b0903ea5927f4574fccf20287faa0e5f875
MrHaribo <jonas_biedermann@hotmail.com>
2013-10-31 13:00:57 +0100

    Erste Version von LinkList
    Basic list behavior with foreach
    Basic class for Linkbean

---
+++ src/linkbean.java
@@ -0,0 +1,38 @@
+
+import java.util.Date;
+
+public class linkbean {
+
+	private String title;
+	private String url;
+	int rating;
+	Date crationDate;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Date getCrationDate() {
+		return crationDate;
+	}
+	public void setCrationDate(Date crationDate) {
+		this.crationDate = crationDate;
+	}
+
+	
+	
+}
---
+++ src/linklist.java
@@ -0,0 +1,12 @@
+
+public class linklist {
+	private String entries[] = { "hallo", "dies", "ist", "ein", "test" };
+
+	public String[] getEntries() {
+		return entries;
+	}
+
+	public void setEntries(String[] entries) {
+		this.entries = entries;
+	}
+}
