1bf3fb6d9592c7cb39c5c2e7d1b59517a2199585
MrHaribo <jonas_biedermann@hotmail.com>
2013-11-06 11:20:31 +0100

    Link Flow with Bean

--- src/rClone/hsr/ch/Linklist.java
+++ src/rClone/hsr/ch/Linklist.java
@@ -1,5 +1,6 @@
 package rClone.hsr.ch;
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,15 +13,18 @@ public class Linklist {
 		links = new LinkedList<Link>();
 		
 		Link l1 = new Link();
-		Link l2 = new Link();
-		Link l3 = new Link();
-		Link l4 = new Link();
-		Link l5 = new Link();
-		
 		l1.setTitle("link1");
+		
+		Link l2 = new Link();
 		l2.setTitle("link2");
+		
+		Link l3 = new Link();
 		l3.setTitle("link3");
+		
+		Link l4 = new Link();
 		l4.setTitle("link4");
+		
+		Link l5 = new Link();
 		l5.setTitle("link5");
 		
 		links.add(l1);
