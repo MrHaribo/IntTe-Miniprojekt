8d812e1d169be8b31e997c1a73e95371717b1e2c
mmeili <mmeili@hsr.ch>
2013-11-06 13:58:54 +0100

    commentlist added
    commentlist.xhtml missing

---
+++ src/rClone/hsr/ch/Comment.java
@@ -0,0 +1,30 @@
+package rClone.hsr.ch;
+
+import java.util.Date;
+
+public class Comment {
+	
+	private String text;
+	int rating;
+	Date creationDate;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+}
---
+++ src/rClone/hsr/ch/Commentsubmit.java
@@ -0,0 +1,35 @@
+package rClone.hsr.ch;
+
+import java.util.Date;
+import java.util.List;
+
+public class Commentsubmit {
+	
+	private String text;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String addComment(){
+		
+		Comment newComment = new Comment();
+		newComment.setCreationDate(new Date());
+		newComment.setRating(0);
+		newComment.setText(text);
+		comments.add(newComment);
+
+		return "commentsubmit";
+	}
+	
+	private List<Comment> comments;
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+}
