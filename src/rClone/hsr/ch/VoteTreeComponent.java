f5b00acaf037499d5eb7e932bddbd8b97dee2f3e
MrHaribo <jonas_biedermann@hotmail.com>
2013-11-16 10:31:59 +0100

    EGit statt dem Github Tool Ganz nebenbei Custom Tags und Custom Components Custom Tags: gehen für xhtml code und Component Klassen

---
+++ src/rClone/hsr/ch/VoteTreeComponent.java
@@ -0,0 +1,33 @@
+package rClone.hsr.ch;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+public class VoteTreeComponent extends UIComponentBase
+{
+
+  public void encodeBegin(FacesContext context) throws IOException
+  {
+   ResponseWriter writer = context.getResponseWriter();
+   String hellomsg = (String)getAttributes().get("hellomsg");
+   
+   writer.startElement("h3", this);
+   if(hellomsg != null)
+     writer.writeText(hellomsg, "hellomsg");
+   else
+     writer.writeText("Hello from a custom JSF UI Component!", null);           
+   writer.endElement("h3");   
+   writer.startElement("p", this);
+   writer.writeText(" Today is: " + new Date(), null);
+   writer.endElement("p");   
+  }
+
+ public String getFamily()
+ {
+  return "HelloFamily";
+ }
+}  
